013ab71e83d8f1f54348024fb4fb012b8642a09c
Michel Daviot <michel@daviot.info>
2019-07-17 14:57:21 +0200

    inline

--- src/main/scala/com/colisweb/skills/Skills.scala
+++ src/main/scala/com/colisweb/skills/Skills.scala
@@ -6,35 +6,23 @@ object Skills {
 
   def isThereAMatchBetween(l1: List[SkillConstraint], l2: List[SkillConstraint]): Boolean = {
 
-    def ownedSkillMatch(skill: String, l: List[SkillConstraint]): Boolean = {
-      val hasSameSkillOfTypeForbidden = l.contains(Forbidden(skill))
+    def ownedSkillMatch(skill: String, l: List[SkillConstraint]): Boolean =
+      !l.contains(Forbidden(skill))
 
-      !hasSameSkillOfTypeForbidden
-    }
+    def forbiddenSkillMatch(skill: String, l: List[SkillConstraint]): Boolean =
+      !l.contains(Owned(skill)) && !l.contains(Required(skill))
 
-    def forbiddenSkillMatch(skill: String, l: List[SkillConstraint]): Boolean = {
-      val hasSameSkillOfTypeOwned = l.contains(Owned(skill))
-      val hasSameSkillOfTypeRequired = l.contains(Required(skill))
-
-      !hasSameSkillOfTypeOwned && !hasSameSkillOfTypeRequired
-    }
-
-    def requiredSkillMatch(skill: String, l: List[SkillConstraint]): Boolean = {
-      val hasSameSkillOfTypeOwned    = l.contains(Owned(skill))
-      val hasSameSkillOfTypeRequired = l.contains(Required(skill))
-      val hasSameSkillOfTypeForbidden = l.contains(Forbidden(skill))
-
-      (hasSameSkillOfTypeOwned || hasSameSkillOfTypeRequired) && !hasSameSkillOfTypeForbidden
-    }
+    def requiredSkillMatch(skill: String, l: List[SkillConstraint]): Boolean =
+      (l.contains(Owned(skill)) || l.contains(Required(skill))) && !l.contains(Forbidden(skill))
 
     @tailrec
     def rec(a: List[SkillConstraint], b: List[SkillConstraint]): Boolean =
       a match {
-        case Nil                                                     => true
-        case Owned(skill) :: _ if !ownedSkillMatch(skill, b)         => false
-        case Required(skill) :: _ if !requiredSkillMatch(skill, b)   => false
+        case Nil => true
+        case Owned(skill) :: _ if !ownedSkillMatch(skill, b) => false
+        case Required(skill) :: _ if !requiredSkillMatch(skill, b) => false
         case Forbidden(skill) :: _ if !forbiddenSkillMatch(skill, b) => false
-        case _ :: t                                                  => rec(t, b)
+        case _ :: t => rec(t, b)
       }
 
     rec(l1, l2) && rec(l2, l1)
