480b49b4d11a60b70fa17551ecb5c8c3cadeabea
Michel Daviot <michel@daviot.info>
2019-07-17 15:04:11 +0200

    simplify recursion

--- src/main/scala/com/colisweb/skills/SkillConstraint.scala
+++ src/main/scala/com/colisweb/skills/SkillConstraint.scala
@@ -1,6 +1,20 @@
 package com.colisweb.skills
 
-sealed trait SkillConstraint
-final case class Owned(skill: String)     extends SkillConstraint
-final case class Required(skill: String)  extends SkillConstraint
-final case class Forbidden(skill: String) extends SkillConstraint
+sealed trait SkillConstraint {
+  def matches(l: List[SkillConstraint]): Boolean
+}
+
+final case class Owned(skill: String) extends SkillConstraint {
+  def matches(l: List[SkillConstraint]): Boolean =
+    !l.contains(Forbidden(skill))
+}
+
+final case class Required(skill: String) extends SkillConstraint {
+  def matches(l: List[SkillConstraint]): Boolean =
+    (l.contains(Owned(skill)) || l.contains(Required(skill))) && !l.contains(Forbidden(skill))
+}
+
+final case class Forbidden(skill: String) extends SkillConstraint {
+  def matches(l: List[SkillConstraint]): Boolean =
+    !l.contains(Owned(skill)) && !l.contains(Required(skill))
+}
--- src/main/scala/com/colisweb/skills/Skills.scala
+++ src/main/scala/com/colisweb/skills/Skills.scala
@@ -1,31 +1,8 @@
 package com.colisweb.skills
 
-import scala.annotation.tailrec
-
 object Skills {
 
-  def isThereAMatchBetween(l1: List[SkillConstraint], l2: List[SkillConstraint]): Boolean = {
-
-    def ownedSkillMatch(skill: String, l: List[SkillConstraint]): Boolean =
-      !l.contains(Forbidden(skill))
-
-    def forbiddenSkillMatch(skill: String, l: List[SkillConstraint]): Boolean =
-      !l.contains(Owned(skill)) && !l.contains(Required(skill))
-
-    def requiredSkillMatch(skill: String, l: List[SkillConstraint]): Boolean =
-      (l.contains(Owned(skill)) || l.contains(Required(skill))) && !l.contains(Forbidden(skill))
-
-    @tailrec
-    def rec(a: List[SkillConstraint], b: List[SkillConstraint]): Boolean =
-      a match {
-        case Nil => true
-        case Owned(skill) :: _ if !ownedSkillMatch(skill, b) => false
-        case Required(skill) :: _ if !requiredSkillMatch(skill, b) => false
-        case Forbidden(skill) :: _ if !forbiddenSkillMatch(skill, b) => false
-        case _ :: t => rec(t, b)
-      }
-
-    rec(l1, l2) && rec(l2, l1)
-  }
+  def isThereAMatchBetween(l1: List[SkillConstraint], l2: List[SkillConstraint]): Boolean =
+    l1.forall(_.matches(l2)) && l2.forall(_.matches(l1))
 
 }
