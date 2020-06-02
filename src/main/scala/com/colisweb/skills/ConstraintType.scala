426462ad4692c5c7817f3fcadbe6777eb9d66a4f
Claire Mielcarek <claire.mielcarek@colisweb.com>
2020-06-02 18:17:00 +0200

    feat: add ConstraintType conversion from and into string

--- src/main/scala/com/colisweb/skills/ConstraintType.scala
+++ src/main/scala/com/colisweb/skills/ConstraintType.scala
@@ -1,7 +1,32 @@
 package com.colisweb.skills
 
-sealed trait ConstraintType
+sealed trait ConstraintType {
+  import ConstraintType._
+
+  override def toString(): String =
+    this match {
+      case Owned     => OWNED
+      case Required  => REQUIRED
+      case Forbidden => FORBIDDEN
+    }
+}
 
 final case object Owned     extends ConstraintType
 final case object Required  extends ConstraintType
 final case object Forbidden extends ConstraintType
+
+object ConstraintType {
+
+  val OWNED     = "owned"
+  val REQUIRED  = "required"
+  val FORBIDDEN = "forbidden"
+
+  def fromString(string: String): Option[ConstraintType] = {
+    string match {
+      case OWNED     => Some(Owned)
+      case REQUIRED  => Some(Required)
+      case FORBIDDEN => Some(Forbidden)
+      case _         => None
+    }
+  }
+}
