088990e49835595b970a1b4c63e4c2e212b3afb3
Scala Steward <gitlab@colisweb.com>
2021-04-26 01:09:57 +0000

    Update scalatest to 3.2.8

--- project/TestDependencies.scala
+++ project/TestDependencies.scala
@@ -4,7 +4,7 @@ object TestVersions {
   final lazy val approval    = "0.13.1"
   final lazy val pprint      = "0.6.4"
   final lazy val scalaCompat = "2.4.2"
-  final lazy val scalatest   = "3.2.7"
+  final lazy val scalatest   = "3.2.8"
 }
 
 object TestDependencies {
