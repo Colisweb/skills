e18227f4ab82dd45dad9896449e30b822f03e7fd
Scala Steward <gitlab@colisweb.com>
2021-05-21 22:09:14 +0000

    Update pprint to 0.6.6

--- project/TestDependencies.scala
+++ project/TestDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object TestVersions {
   final lazy val approval    = "0.13.2"
-  final lazy val pprint      = "0.6.5"
+  final lazy val pprint      = "0.6.6"
   final lazy val scalaCompat = "2.4.4"
   final lazy val scalatest   = "3.2.9"
 }
